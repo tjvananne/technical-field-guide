3570ee2055d2e8bebf78ac995c129c3d304a5314
Van Anne <tvananne@txt.textron.com>
2017-06-22 17:03:44 -0500

    adding gradient descent file from a long time ago

--- undigested_content/gradient_descent_in_R.R
+++ undigested_content/gradient_descent_in_R.R
@@ -6,6 +6,21 @@
 # https://sw23993.wordpress.com/2017/02/12/implementing-the-gradient-descent-algorithm-in-r/ 
 
 
+#' The goal of machine learning is to figure out a function f(x) that maps a value "x" to a
+#' corresponding y value (the label)
+#' 
+#' In order to figure out how effective our function f(x) is... we need a way to measure
+#' it's predictive capability relative to other functions. For this, we need a "cost function" 
+#' which we'll call J(). 
+#' 
+#' For simple linear regression problems, we can find the best fit line using a least squares
+#' calculation, but we'll show how a more complicated J() Cost function would work on a linear model.
+#' The J() cost function we'll use here is called "gradient descent" and it uses calculus / derivatives
+#' in order to A) determine what changes 
+
+
+
+
 # his definition of the line equation is using these variables:
 # y = mx + c    (m is slope coefficient, x is variable, c is intercept)
 
@@ -42,7 +57,7 @@ gradientDesc <- function(x, y, learn_rate, conv_threshold, n, max_iter) {
 }
 
 
-my_gradientDesc <- function(x, y, learn_rate, conv_threshold, n, max_iter) {
+my_gradientDesc <- function(x, y, learn_rate, conv_threshold, n, max_iter, print_every_n=30000) {
     plot(x, y, col = "blue", pch = 20)
     m <- runif(1, 0, 1)
     c <- runif(1, 0, 1)
@@ -56,7 +71,7 @@ my_gradientDesc <- function(x, y, learn_rate, conv_threshold, n, max_iter) {
                 # optional- create new plot with number of iterations listed
                 # mark the current best fit line
             # or just add the line to existing plot so we have a ton of lines
-        if(iterations %% 30000 == 0) {
+        if(iterations %% print_every_n == 0) {
             plot(x, y, col = "blue", pch = 20, main=paste0("iteration: ", iterations))
             abline(c, m)
             Sys.sleep(1)
@@ -91,6 +106,20 @@ my_gradientDesc <- function(x, y, learn_rate, conv_threshold, n, max_iter) {
 # Run the function 
 set.seed(1)
 #gradientDesc(mtcars$disp, mtcars$mpg, 0.0000293, 0.001, 32, 2500000)
-my_gradientDesc(mtcars$disp, mtcars$mpg, 0.0000293, 0.001, 32, 2500000)
+my_gradientDesc(x=mtcars$disp, 
+                y=mtcars$mpg, 
+                learn_rate=0.0000293, 
+                conv_threshold=0.001, 
+                n=32, 
+                max_iter=2500000,
+                print_every_n=30000)
 
+
+my_gradientDesc(x=mtcars$disp, 
+                y=mtcars$mpg, 
+                learn_rate=0.0000293, 
+                conv_threshold=0.001, 
+                n=32, 
+                max_iter=2500000,
+                print_every_n=1000)
 
